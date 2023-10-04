7f2e0ac6edf4b8010fe02f8b51075ba8d0cead72
emmapeter <emmanuelodion48@gmail.com>
2023-10-04 04:06:42 -0700

    second commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var mean = document.getElementById("mysidenav");
+
+function openNav() {
+    mean.classList.add("slide")
+    
+};
+
+function closeNav() {
+    mean.classList.remove("slide")
+    
+};
